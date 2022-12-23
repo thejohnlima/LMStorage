7f535054383f5bbb0b4db02c0d65675bdce0c7a5
John Lima <thejohnlima@icloud.com>
2022-12-22 21:34:06 -0500

    Update Swift version and fastlane script

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 
 import PackageDescription
 
@@ -7,23 +7,20 @@ let package = Package(
   platforms: [
     .macOS(.v10_13),
     .iOS(.v12),
-    .watchOS(.v3),
-    .tvOS(.v9)
+    .watchOS(.v4),
+    .tvOS(.v11)
   ],
   products: [
-    .library(
-      name: "LMStorage",
-      targets: ["LMStorage"]
-    )
+    .library(name: "LMStorage", targets: ["LMStorage"])
   ],
   dependencies: [
-    .package(name: "KeychainSwift", url: "https://github.com/evgenyneu/keychain-swift.git", from: "19.0.0")
+    .package(url: "https://github.com/evgenyneu/keychain-swift.git", from: "20.0.0")
   ],
   targets: [
     .target(
       name: "LMStorage",
       dependencies: [
-        "KeychainSwift"
+        .product(name: "KeychainSwift", package: "keychain-swift")
       ]
     ),
     .testTarget(
